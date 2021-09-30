f511a8966fedbe9722eb25687f89e1626efaa054
Raxmatov Eldorbek <raxmatoveldor37@gmail.com>
2021-09-30 08:49:10 +0500

    old router exchanged new router mongodb

--- routers/router_data.js
+++ routers/router_data.js
@@ -1,9 +1,12 @@
 // kerakli package va file dan obj olamiz...
-const { Router } = require('express');
-const readController = require('../controllers/send_datas');
+const { Router } = require("express");
+// const readController = require('../controllers/send_datas');
+const readControllerMongo = require("../controllers/send_mongodatas");
+const ControllerMongo = require("../controllers/crud_mongodatas");
 
 const router = Router();
 
-router.get('/api/readData', readController.readData);
-
+// router.get('/api/readData', readController.readData);
+router.get("/api/readData", readControllerMongo.readData);
+router.post("/api/PopularData", ControllerMongo.createPopularData);
 module.exports = router;
