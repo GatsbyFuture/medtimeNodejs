aec1002bc735a756a3c61a501390b3d7606eb47a
Raxmatov Eldorbek <raxmatoveldor37@gmail.com>
2021-10-04 09:44:45 +0500

    added new file and build admin panel

--- midllewares/startup.js
+++ midllewares/startup.js
@@ -2,25 +2,30 @@
 const compression = require("compression");
 const express = require("express");
 const helmet = require("helmet");
+const cookieParser = require("cookie-parser");
 // const morgan = require("morgan");
 var cors = require("cors");
 const path = require("path");
 const readRouter = require("../routers/router_data");
 
 // export qilib qo'yamiz use f(x)larini...
-module.exports = function (app) {
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
-  app.use(express.static("public"));
-  app.use(express.static(path.join(__dirname, "public")));
-  // qaysi muhitda ishlayotganini bilish uchun...
-  // if (app.get("env") == "development") {
-  //   app.use(morgan("tiny"));
-  //   console.log("dastur devlopment muhitida ishlayapti...");
-  // }
-  app.use(cors());
-  app.use(helmet());
-  app.use(compression());
-
-  app.use("/data", readRouter);
+module.exports = function(app) {
+    app.set("views", path.join(__dirname, "views"));
+    app.set("view engine", "ejs");
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+    app.use(cookieParser());
+    app.use(express.static(path.join(__dirname, "public")));
+    // qaysi muhitda ishlayotganini bilish uchun...
+    // if (app.get("env") == "development") {
+    //   app.use(morgan("tiny"));
+    //   console.log("dastur devlopment muhitida ishlayapti...");
+    // }
+    app.use(cors());
+    app.use(helmet());
+    app.use(compression());
+    // login pagega kirish uchun
+    app.use("/", loginRoter);
+    // user uchun asosiy CRUD ishlari...
+    app.use("/data", readRouter);
 };
