0f569ecb54818cd83b2f7cfdc607833c83cb9b5b
Raxmatov Eldorbek <raxmatoveldor37@gmail.com>
2021-10-04 15:32:05 +0500

    Uptade

--- routers/router_admin.js
+++ routers/router_admin.js
@@ -4,15 +4,15 @@ const router = Router();
 
 // admin panelni yasash uchun ejs filellaridan foydalanamiz...
 router.get("/login", function(req, res, next) {
-    res.sendFile("../views/login");
+    res.sendFile(__dirname + "/../views/login");
 });
 // asosiy data to'ldirish menusi...
 router.get("/add", function(req, res, next) {
-    res.render("form");
+    res.render(__dirname + "/../views/form");
 });
 // datani chiqarish uchun page..
 router.get("/view", function(req, res, next) {
-    res.render("tables");
+    res.render(__dirname + "/../views/tables");
 });
 
 module.exports = router;
